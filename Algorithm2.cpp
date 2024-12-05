e0f38ebeaf803e5632a1b45b920d19301ca04279
dheeh4755 <dheeh4755@gmail.com>
2024-12-05 17:32:30 +0100

    Update Algorithm2.cpp

--- Algorithm2.cpp
+++ Algorithm2.cpp
@@ -1,83 +1,7 @@
 #include "Algorithm2.h"
 
-#include <CGAL/Triangular_expansion_visibility_2.h>
-#include <CGAL/Simple_polygon_visibility_2.h>
-#include <tuple>
-#include <CGAL/Boolean_set_operations_2.h>
-#include <optional>
 
-
-typedef CGAL::Triangular_expansion_visibility_2<Arrangement_2>              VA_type;
-typedef CGAL::Simple_polygon_visibility_2<Arrangement_2, CGAL::Tag_false>   VP_type_non_regular_face;
-typedef Arrangement_2::Face_const_handle                                    Face_const_handle;
-
-
-
-Arrangement_2 computeVisibilityArrangementAtEdge(const Arrangement_2 & A, const Point& p, Halfedge_const_handle e) {
-    Arrangement_2 VA;
-    VA_type VP_calculator(A);
-    VP_calculator.compute_visibility(p, e, VA);
-    return VA;
-}
-
-Arrangement_2 computeVisibilityArrangementAtFace(const Arrangement_2 & A, const Point& p) {
-    Face_const_handle f;
-    if (A.edges_begin()->face()->is_unbounded()){
-        f = A.edges_begin()->twin()->face();
-    }
-    else {
-        f = A.edges_begin()->face();
-    }
-
-    Arrangement_2 VA;
-    VP_type_non_regular_face VP_calculator(A);
-    VP_calculator.compute_visibility(p, f, VA);
-
-    return VA;
-}
-
-Arrangement_2 computeVisibilityArrangementGeneral(const Arrangement_2& A, const Point& p) {
-    Arrangement_2 VP;
-
-    for (auto eA: getEdgesOfArrangement(A)) {
-        if (pointIsOnEdgeButNotSource(p, eA)) {
-            VP = computeVisibilityArrangementAtEdge(A, p, eA);
-            break;
-        }
-    }
-
-    if (VP.is_empty()) {
-        VP = computeVisibilityArrangementAtFace(A, p);
-    }
-    return VP;
-}
-
-
-std::vector<Point> findAllIntersectionsBetweenEdgesOfPolygons(const Polygon_2& P, const Polygon_2& Q){
-    std::vector<Point> I;
-    for (auto eP: P.edges()) {
-        for (auto eQ: Q.edges()) {
-            auto intersection = CGAL::intersection(eP, eQ);
-            if (intersection) {
-                const Point *pI = std::get_if<Point>(&*intersection);
-                I.push_back(*pI);
-            }
-        }
-    }
-    return I;
-}
-
-bool passedStart(const Halfedge_circulator e, const std::optional<Point>& start, const Point p) {
-    if (start.has_value() and pointIsOnEdgeButNotSource(start.value(), e)){
-        // check if 'p' is closer than 'start' to 'e''s target
-        if (CGAL::squared_distance(p, e->target()->point()) <= CGAL::squared_distance(start.value(), e->target()->point())){
-            return true;
-        }
-    }
-    return false;
-}
-
-std::tuple<Point, Halfedge_circulator, Point, bool> greedyStep(const Arrangement_2& A, Halfedge_circulator e, Point p, const std::optional<Point>& start) {
+std::tuple<Point, Halfedge_circulator, Point, bool> greedyInterval(const Arrangement_2& A, Halfedge_circulator e, Point p, const std::optional<Point>& start) {
     bool isFinished = false;
 
     Arrangement_2 F1 = computeVisibilityArrangementAtEdge(A, p, e);
@@ -89,9 +13,9 @@ std::tuple<Point, Halfedge_circulator, Point, bool> greedyStep(const Arrangement
         e++;
     }
 
-    // #==============================================#
-    // #  1) EXPAND THE GREEDY STEP BY ENTIRE EDGES   #
-    // #==============================================#
+    // #==================================================#
+    // #  1) EXPAND THE GREEDY INTERVAL BY ENTIRE EDGES   #
+    // #==================================================#
 
     int i = 0;
     for (; i < A.number_of_vertices() + 2; i++){
@@ -124,14 +48,14 @@ std::tuple<Point, Halfedge_circulator, Point, bool> greedyStep(const Arrangement
         e++;
     }
 
-    // If greedyStep can traverse the entire polygon boundary then it is trivially solved by one guard
+    // If greedyInterval can traverse the entire polygon boundary then it is trivially solved by one guard
     if (i == A.number_of_vertices() + 2){
         return std::make_tuple(p, e, p, true);
         }
 
-    // #===============================================#
-    // #  2) EXPAND THE GREEDY STEP ON THE FINAL EDGE  #
-    // #===============================================#
+    // #===================================================#
+    // #  2) EXPAND THE GREEDY INTERVAL ON THE FINAL EDGE  #
+    // #===================================================#
 
     Point pFirst = e->source()->point();
     Point pBest = e->source()->point();
@@ -178,9 +102,4 @@ std::tuple<Point, Halfedge_circulator, Point, bool> greedyStep(const Arrangement
 
     return std::make_tuple(guard, e, pBest, isFinished);
 }
-
-
-
-
-
 
