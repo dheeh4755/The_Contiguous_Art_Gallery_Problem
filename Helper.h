ce8aa7453d3c2e9854199bd6e0f81f3767430b91
dheeh4755 <dheeh4755@gmail.com>
2024-12-05 17:31:40 +0100

    Update Helper.h

--- Helper.h
+++ Helper.h
@@ -8,18 +8,34 @@
 #include <CGAL/Arr_segment_traits_2.h>
 #include <CGAL/Polygon_set_2.h>
 #include <CGAL/Polygon_with_holes_2.h>
+#include <CGAL/Triangular_expansion_visibility_2.h>
+#include <CGAL/Simple_polygon_visibility_2.h>
+#include <CGAL/Boolean_set_operations_2.h>
 
-typedef CGAL::Exact_predicates_exact_constructions_kernel   Kernel;
-typedef Kernel::Point_2                                     Point;
-typedef Kernel::Segment_2                                   Segment;
-typedef CGAL::Arr_segment_traits_2<Kernel>                  Traits;
-typedef CGAL::Arrangement_2<Traits>                         Arrangement_2;
-typedef CGAL::Polygon_2<Kernel>                             Polygon_2;
-typedef CGAL::Polygon_set_2<Kernel>                         Polygon_set_2;
-typedef CGAL::Polygon_with_holes_2<Kernel>                  Polygon_with_holes_2;
-typedef Arrangement_2::Ccb_halfedge_const_circulator        Halfedge_circulator;
-typedef Arrangement_2::Halfedge_const_handle                Halfedge_const_handle;
+typedef CGAL::Exact_predicates_exact_constructions_kernel                   Kernel;
+typedef Kernel::Point_2                                                     Point;
+typedef Kernel::Segment_2                                                   Segment;
+typedef CGAL::Arr_segment_traits_2<Kernel>                                  Traits;
+typedef CGAL::Arrangement_2<Traits>                                         Arrangement_2;
+typedef CGAL::Polygon_2<Kernel>                                             Polygon_2;
+typedef CGAL::Polygon_set_2<Kernel>                                         Polygon_set_2;
+typedef CGAL::Polygon_with_holes_2<Kernel>                                  Polygon_with_holes_2;
+typedef Arrangement_2::Ccb_halfedge_const_circulator                        Halfedge_circulator;
+typedef Arrangement_2::Halfedge_const_handle                                Halfedge_const_handle;
+typedef CGAL::Triangular_expansion_visibility_2<Arrangement_2>              VA_type;
+typedef CGAL::Simple_polygon_visibility_2<Arrangement_2, CGAL::Tag_false>   VP_type_non_regular_face;
+typedef Arrangement_2::Face_const_handle                                    Face_const_handle;
 
+
+Arrangement_2 computeVisibilityArrangementAtEdge(const Arrangement_2 & A, const Point& p, Halfedge_const_handle e);
+
+Arrangement_2 computeVisibilityArrangementAtFace(const Arrangement_2 & A, const Point& p);
+
+Arrangement_2 computeVisibilityArrangementGeneral(const Arrangement_2& A, const Point& p);
+
+std::vector<Point> findAllIntersectionsBetweenEdgesOfPolygons(const Polygon_2& P, const Polygon_2& Q);
+
+bool passedStart(const Halfedge_circulator e, const std::optional<Point>& start, const Point p);
 
 bool isInGeneralPosition(const Arrangement_2 & A);
 
